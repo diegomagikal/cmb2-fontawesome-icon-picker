287f53d3a6908cc2498b0c470ea7c42bc2816ee2
Diego Luiz <diego@magikal.com>
2018-11-20 14:39:57 -0300

    moved dependencies to node_modules (yarn / npm support), updated repositories, added support to attach news icons

--- assets/js/fontawesome-picker-init.js
+++ assets/js/fontawesome-picker-init.js
@@ -2,6 +2,19 @@
 jQuery(document).ready(function($) {
   'use strict';
   $('.fontawesome-icon-select').iconpicker({
-    hideOnSelect: true
+    hideOnSelect: true,
+    title: 'Clique no ícone desejado',
+    icons: typeof icones != 'undefined' ?  $.merge(icones, $.iconpicker.defaultOptions.icons) : null,
+        fullClassFormatter: function(val){
+            if(val.match(/^fas-/)) return 'fas '+val;
+            if(val.match(/^fab-/)) return 'fab '+val;
+            if(val.match(/^fa-/)) return 'fa '+val;
+            return val;
+            
+        },
+
+    templates: {
+        search: '<input type="search" class="form-control iconpicker-search" placeholder="Digite para filtrar" />'
+    }
   });
 }); // End Ready
--- cmb2-fontawesome-picker.php
+++ cmb2-fontawesome-picker.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: CMB2 Field Type: FontAwesome Picker
-Plugin URI: https://github.com/thekhorshed/cmb2-fontawesome-picker
+Plugin URI: https://github.com/diegomagikal/cmb2-fontawesome-picker
 Description: Font-Awesome Icon picker field type for CMB2
-Version: 1.0.0
-Author: Khorshed Alam
+Version: 1.1.0
+Author: Khorshed Alam / Changes by Diego Luiz
 Author URI: http://khorshedalam.com
 License: GPLv2+
 */
@@ -17,7 +17,7 @@ class KS_FontAwesome_IconPicker {
     /**
      * Current version number
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
 
     /**
      * Initialize the plugin by hooking into CMB2
@@ -40,9 +40,7 @@ class KS_FontAwesome_IconPicker {
      * Sanitize icon class name
      */
    public function sanitize( $sanitized_val, $val ) {
-        if ( ! empty( $val ) ) {
-            return  sanitize_html_class( $val );
-        }
+        //sanitize disabled due to multiple icon prefix (fas, fab, fa, etc)
         return $sanitized_val;
     }
 
@@ -72,12 +70,45 @@ class KS_FontAwesome_IconPicker {
             'jquery',
         );
 
-    	wp_enqueue_script( 'cmb2-fontawesome-picker', $url . 'assets/js/fontawesome-iconpicker.min.js', array('jquery'), self::VERSION, true );
+    	wp_enqueue_script( 'cmb2-fontawesome-picker', $url . 'node_modules/fontawesome-iconpicker/dist/js/fontawesome-iconpicker.min.js', array('jquery'), self::VERSION, true );
         wp_enqueue_script( 'cmb2-fontawesome-picker-init', $url . 'assets/js/fontawesome-picker-init.js', array('cmb2-fontawesome-picker'), self::VERSION, true );
-    	wp_enqueue_style( 'cmb2-fontawesome-css', $url . 'assets/css/font-awesome.min.css', array(), self::VERSION );
+    	
+        wp_enqueue_style( 'cmb2-fontawesome-css', $url . 'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css', array(), self::VERSION );
+        wp_enqueue_style( 'cmb2-fontawesome-css-all', $url . 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', array(), self::VERSION );
         wp_enqueue_style( 'bootstrap-popovers', $url . 'assets/css/bootstrap-popovers.css', array('cmb2-fontawesome-css'), self::VERSION );
-        wp_enqueue_style( 'cmb2-fontawesome-picker', $url . 'assets/css/fontawesome-iconpicker.min.css', array('bootstrap-popovers'), self::VERSION );
+       
+        wp_enqueue_style( 'cmb2-fontawesome-picker', $url . 'node_modules/fontawesome-iconpicker/dist/css/fontawesome-iconpicker.min.css', array('bootstrap-popovers'), self::VERSION );
         wp_enqueue_style( 'cmb2-fontawesome-picker-fixes', $url . 'assets/css/cmb2-fixes.css', array('cmb2-fontawesome-picker'), self::VERSION );
+    }
+
+
+    /**
+     * load the css int the frontend (needs call)
+     * Ex: (new KS_FontAwesome_IconPicker())->scripts_frontend(); 
+     */
+    public function scripts_frontend() {
+        $dir = trailingslashit( dirname( __FILE__ ) );
+
+        if ( 'WIN' === strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
+            // Windows
+            $content_dir = str_replace( '/', DIRECTORY_SEPARATOR, WP_CONTENT_DIR );
+            $content_url = str_replace( $content_dir, WP_CONTENT_URL, $dir );
+            $url = str_replace( DIRECTORY_SEPARATOR, '/', $content_url );
+        }
+        else {
+            $url = str_replace(
+                array( WP_CONTENT_DIR, WP_PLUGIN_DIR ),
+                array( WP_CONTENT_URL, WP_PLUGIN_URL ),
+                $dir
+            );
+        }
+
+        $url = set_url_scheme( $url );
+
+       
+        
+        wp_enqueue_style( 'cmb2-fontawesome-css', $url . 'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css', array(), self::VERSION );
+        wp_enqueue_style( 'cmb2-fontawesome-css-all', $url . 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', array(), self::VERSION );
     }
 }
 new KS_FontAwesome_IconPicker();
